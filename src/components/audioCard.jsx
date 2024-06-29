f3b93e2def0efad3552900d820831d25bc3e9c89
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-06-29 16:42:14 +0500

    add audio

---
+++ src/components/audioCard.jsx
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+import { VscDebugStart } from "react-icons/vsc";
+import { HiOutlineBookmark } from "react-icons/hi";
+import { VscDebugPause } from "react-icons/vsc";
+import { useRef, useState } from "react";
+import Border from '../assets/icons/border.svg'
+
+
+const AudioCard = ({el}) => {
+    console.log(el);    
+    const audioRefs = useRef([]);
+    const [playingIndex, setPlayingIndex] = useState(null);
+    const playAudio = (index) => {
+        console.log(index, playingIndex);
+        console.log(index==playingIndex);
+        if (playingIndex !== null && playingIndex == index) {
+            audioRefs.current[playingIndex].pause();
+            audioRefs.current[playingIndex].currentTime = 0;
+        }
+        audioRefs.current[index].play();
+        setPlayingIndex(index);
+    };
+
+    const pauseAudio = (index) => {
+        audioRefs.current[index].pause();
+        setPlayingIndex(null);
+    };
+    
+    // const stopAudio = () => {
+    //     audioRefs.current.pause();
+    //     audioRefs.current.currentTime = 0;
+    // };
+    return (
+        <div key={el.nomor} className="card mt-10 h-16 cursor-pointer flex justify-between items-center">
+            <span className="block bg-[#9543FF] dark:bg-white h-16 rounded-lg w-2"></span>
+            <audio ref={elRef => audioRefs.current[el.nomor] = elRef} className="rounded-none" src={el.audioFull['01']}></audio>
+            <div className="main-info w-[95%] flex justify-between dark:text-white items-center hover:shadow-2xl p-2 py-4 rounded-md">
+                <div className="flex items-center gap-x-4">
+                    <div className="relative text-center">
+                        <img className="w-[50px]" src={Border} alt="" />
+                        <p className={`absolute top-[12px] font-bold ${el.nomor < 9 ? 'left-5' : el.nomor < 99 ? 'left-4' : 'left-3'} text-center`}>{el.nomor}</p>
+                    </div>
+                    <div className="">
+                        <h2 className="embad text-[20px] font-semibold">{el.namaLatin}</h2>
+                        <h3 className="embad text-[15px] font-semibold text-[#8789A3] uppercase">{el.tempatTurun} {el.jumlahAyat} Ayat</h3>
+                    </div>
+                </div>
+                <div className="audio pr-2">
+                    <h2 className="arab text-[20px] font-bold">{el.nama}</h2>
+                    <button onClick={() => playAudio(el.nomor)} className={playingIndex !== el.nomor ? "mr-2" : "hidden"}><VscDebugStart size={20} /></button>
+                    <button onClick={() => pauseAudio(el.nomor)} className={playingIndex === el.nomor ? "mr-2" : "hidden"}><VscDebugPause size={20} /></button>
+                    <button><HiOutlineBookmark size={20} /></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+AudioCard.propTypes = {
+    el: PropTypes.shape({
+        nomor: PropTypes.number.isRequired,
+        audioFull: PropTypes.object.isRequired,
+        namaLatin: PropTypes.string.isRequired,
+        tempatTurun: PropTypes.string.isRequired,
+        jumlahAyat: PropTypes.number.isRequired,
+        nama: PropTypes.string.isRequired
+    }).isRequired
+};
+
+export default AudioCard;
--- src/pages/quran.jsx
+++ src/pages/quran.jsx
@@ -3,11 +3,7 @@ import { QuranUtils } from "../utils/quran.utils";
 import Loading from "../components/loading";
 import QuranImg from '../assets/images/quranhone.png'
 import { Link } from "react-router-dom";
-import Border from '../assets/icons/border.svg'
-import { VscDebugStart } from "react-icons/vsc";
-import { HiOutlineBookmark } from "react-icons/hi";
-import { VscDebugPause } from "react-icons/vsc";
-import { useRef } from "react";
+import AudioCard from "../components/audioCard";
 
 
 const Quran = () => {
@@ -15,21 +11,6 @@ const Quran = () => {
         queryKey: ['quranAll'],
         queryFn: QuranUtils.getQuran
     })
-    const audioQuran = useRef(null)
-    console.log(data?.data);
-    console.dir(audioQuran.current);
-    const playAudio = () => {
-        audioQuran.current.play();
-      };
-    
-      const pauseAudio = () => {
-        audioQuran.current.pause();
-      };
-    
-      const stopAudio = () => {
-        audioQuran.current.pause();
-        audioQuran.current.currentTime = 0;
-      };
     if(isLoading) return <Loading/>
     return (
         <div className="container overflow-hidden bg-white dark:bg-[#180B37]">
@@ -45,27 +26,7 @@ const Quran = () => {
             </div>
             <div className="body-quran px-2">
                 {data?.data.length && data.data.map(el => {
-                    return <div key={el.nomor} className="card mt-10 h-16 cursor-pointer flex justify-between items-center">
-                                <span className="block bg-[#9543FF] dark:bg-white h-16 rounded-lg w-2"></span>
-                                <div className="main-info w-[95%] flex justify-between dark:text-white items-center hover:shadow-2xl p-2 py-4 rounded-md">
-                                    <div className="flex items-center gap-x-4">
-                                        <div className="relative text-center">
-                                            <img className="w-[50px]" src={Border} alt="" />
-                                            <p className={`absolute top-[12px] font-bold  ${el.nomor<9?'left-5':el.nomor<99?'left-4':'left-3'} text-center`}>{el.nomor}</p>
-                                        </div>
-                                        <div className="">
-                                            <h2 className="embad text-[20px] font-semibold">{el.namaLatin}</h2>
-                                            <h3 className="embad text-[15px] font-semibold text-[#8789A3] uppercase">{el.tempatTurun} {el.jumlahAyat} Ayat</h3>
-                                        </div>
-                                    </div>
-                                    <div className="audio pr-2">
-                                        <h2 className="arab text-[20px] font-bold">{el.nama}</h2>
-                                        <button onClick={playAudio} className="mr-2"><VscDebugStart size={20}/><audio className="w-full rounded-none" ref={audioQuran} src={el.audioFull['04']}></audio></button>
-                                        <button onClick={pauseAudio} className="mr-2"><VscDebugPause size={20}/></button>
-                                        <button><HiOutlineBookmark size={20}/></button>
-                                    </div>
-                                </div>                                
-                            </div>
+                    return <AudioCard key={el.nomor} el={el}/>
                 })}
             </div>
         </div>
